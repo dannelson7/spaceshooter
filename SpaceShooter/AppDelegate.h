7fc5a05786bbebff744f4c87c4c62fc3e34b33d8
dnelson <dannelson7@gmail.com>
2014-03-09 15:31:07 -0400

    Initial commit from local git

---
+++ SpaceShooter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpaceShooter
+//
+//  Created by Daniel Nelson on 3/9/14.
+//  Copyright (c) 2014 Daniel Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
