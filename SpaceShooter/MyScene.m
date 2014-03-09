126c8e1f4b8750a18c5ec6c51bd6d251079eb793
dnelson <dannelson7@gmail.com>
2014-03-09 16:58:45 -0400

    Background and motion

--- SpaceShooter/MyScene.m
+++ SpaceShooter/MyScene.m
@@ -6,48 +6,133 @@
 //  Copyright (c) 2014 Daniel Nelson. All rights reserved.
 //
 
+@import CoreMotion;
+
 #import "MyScene.h"
+#import "FMMParallaxNode.h"
 
 @implementation MyScene
+{
+    SKSpriteNode *_ship;    //1
+    FMMParallaxNode *_parallaxNodeBackgrounds;
+    FMMParallaxNode *_parallaxSpaceDust;
+    CMMotionManager *_motionManager;
+}
 
--(id)initWithSize:(CGSize)size {    
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
+        //2
+        NSLog(@"SKScene:initWithSize %f x %f",size.width,size.height);
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        //3
+        self.backgroundColor = [SKColor blackColor];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+#pragma mark - Game Backgrounds
+        //1
+        NSArray *parallaxBackgroundNames = @[@"bg_galaxy.png", @"bg_planetsunrise.png",
+                                             @"bg_spacialanomaly.png", @"bg_spacialanomaly2.png"];
+        CGSize planetSizes = CGSizeMake(200.0, 200.0);
+        //2
+        _parallaxNodeBackgrounds = [[FMMParallaxNode alloc] initWithBackgrounds:parallaxBackgroundNames
+                                                                           size:planetSizes
+                                                           pointsPerSecondSpeed:10.0];
+        //3
+        _parallaxNodeBackgrounds.position = CGPointMake(size.width/2.0, size.height/2.0);
+        //4
+        [_parallaxNodeBackgrounds randomizeNodesPositions];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        //5
+        [self addChild:_parallaxNodeBackgrounds];
         
-        [self addChild:myLabel];
+        //6
+        NSArray *parallaxBackground2Names = @[@"bg_front_spacedust.png",@"bg_front_spacedust.png"];
+        _parallaxSpaceDust = [[FMMParallaxNode alloc] initWithBackgrounds:parallaxBackground2Names
+                                                                     size:size
+                                                     pointsPerSecondSpeed:25.0];
+        _parallaxSpaceDust.position = CGPointMake(0, 0);
+        [self addChild:_parallaxSpaceDust];
+        
+#pragma mark - Setup Sprite for the ship
+        //Create space sprite, setup position on left edge centered on the screen, and add to Scene
+        //4
+        _ship = [SKSpriteNode spriteNodeWithImageNamed:@"SpaceFlier_sm_1.png"];
+        _ship.position = CGPointMake(self.frame.size.width * 0.1, CGRectGetMidY(self.frame));
+        [self addChild:_ship];
+        
+#pragma mark - TBD - Setup the asteroids
+        
+#pragma mark - TBD - Setup the lasers
+        
+#pragma mark - TBD - Setup the Accelerometer to move the ship
+        _motionManager = [[CMMotionManager alloc] init];
+        
+#pragma mark - TBD - Setup the stars to appear as particles
+        [self addChild:[self loadEmitterNode:@"stars1"]];
+        [self addChild:[self loadEmitterNode:@"stars2"]];
+        [self addChild:[self loadEmitterNode:@"stars3"]];
+        
+#pragma mark - TBD - Start the actual game
+        [self startTheGame];
+        
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+- (SKEmitterNode *)loadEmitterNode:(NSString *)emitterFileName
+{
+    NSString *emitterPath = [[NSBundle mainBundle] pathForResource:emitterFileName ofType:@"sks"];
+    SKEmitterNode *emitterNode = [NSKeyedUnarchiver unarchiveObjectWithFile:emitterPath];
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    //do some view specific tweaks
+    emitterNode.particlePosition = CGPointMake(self.size.width/2.0, self.size.height/2.0);
+    emitterNode.particlePositionRange = CGVectorMake(self.size.width+100, self.size.height);
+    
+    return emitterNode;
+}
+
+- (void)startTheGame
+{
+    _ship.hidden = NO;
+    //reset ship position for new game
+    _ship.position = CGPointMake(self.frame.size.width * 0.1, CGRectGetMidY(self.frame));
+    
+    //setup to handle accelerometer readings using CoreMotion Framework
+    [self startMonitoringAcceleration];
+    
+}
+
+- (void)startMonitoringAcceleration
+{
+    if (_motionManager.accelerometerAvailable) {
+        [_motionManager startAccelerometerUpdates];
+        NSLog(@"accelerometer updates on...");
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
+- (void)stopMonitoringAcceleration
+{
+    if (_motionManager.accelerometerAvailable && _motionManager.accelerometerActive) {
+        [_motionManager stopAccelerometerUpdates];
+        NSLog(@"accelerometer updates off...");
+    }
+}
+
+- (void)updateShipPositionFromMotionManager
+{
+    CMAccelerometerData* data = _motionManager.accelerometerData;
+    if (fabs(data.acceleration.x) > 0.2) {
+        NSLog(@"acceleration value = %f",data.acceleration.x);
+    }
+}
+
+-(void)update:(NSTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    
+    //Update background (parallax) position
+    [_parallaxSpaceDust update:currentTime];
+    [_parallaxNodeBackgrounds update:currentTime];
+    [self updateShipPositionFromMotionManager];
 }
 
 @end
