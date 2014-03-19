f3a5cc6ff87bef6b2f08bfea701852e2b9c7469b
dnelson <dannelson7@gmail.com>
2014-03-18 20:18:11 -0400

    Collision Detection

--- SpaceShooter/MyScene.m
+++ SpaceShooter/MyScene.m
@@ -11,12 +11,21 @@
 #import "MyScene.h"
 #import "FMMParallaxNode.h"
 
+#define kNumAsteroids   15
+#define kNumLasers      5
+
 @implementation MyScene
 {
     SKSpriteNode *_ship;    //1
     FMMParallaxNode *_parallaxNodeBackgrounds;
     FMMParallaxNode *_parallaxSpaceDust;
     CMMotionManager *_motionManager;
+    NSMutableArray *_asteroids;
+    int _nextAsteroid;
+    double _nextAsteroidSpawn;
+    NSMutableArray *_shipLasers;
+    int _nextShipLaser;
+    int _lives;
 }
 
 -(id)initWithSize:(CGSize)size {
@@ -27,6 +36,9 @@
         
         //3
         self.backgroundColor = [SKColor blackColor];
+        
+        //Define your physics body around the screen - used by your ship to not bounce off the screen
+        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         
 #pragma mark - Game Backgrounds
         //1
@@ -58,11 +70,39 @@
         //4
         _ship = [SKSpriteNode spriteNodeWithImageNamed:@"SpaceFlier_sm_1.png"];
         _ship.position = CGPointMake(self.frame.size.width * 0.1, CGRectGetMidY(self.frame));
+        //move the ship using Sprite Kit's Physics Engine
+        //1
+        _ship.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_ship.frame.size];
+        
+        //2
+        _ship.physicsBody.dynamic = YES;
+        
+        //3
+        _ship.physicsBody.affectedByGravity = NO;
+        
+        //4
+        _ship.physicsBody.mass = 0.02;
         [self addChild:_ship];
         
 #pragma mark - TBD - Setup the asteroids
+        _asteroids = [[NSMutableArray alloc] initWithCapacity:kNumAsteroids];
+        for (int i = 0; i < kNumAsteroids; ++i) {
+            SKSpriteNode *asteroid = [SKSpriteNode spriteNodeWithImageNamed:@"asteroid"];
+            asteroid.hidden = YES;
+            [asteroid setXScale:0.5];
+            [asteroid setYScale:0.5];
+            [_asteroids addObject:asteroid];
+            [self addChild:asteroid];
+        }
         
 #pragma mark - TBD - Setup the lasers
+        _shipLasers = [[NSMutableArray alloc] initWithCapacity:kNumLasers];
+        for (int i = 0; i < kNumLasers; ++i) {
+            SKSpriteNode *shipLaser = [SKSpriteNode spriteNodeWithImageNamed:@"laserbeam_blue"];
+            shipLaser.hidden = YES;
+            [_shipLasers addObject:shipLaser];
+            [self addChild:shipLaser];
+        }
         
 #pragma mark - TBD - Setup the Accelerometer to move the ship
         _motionManager = [[CMMotionManager alloc] init];
@@ -93,9 +133,19 @@
 
 - (void)startTheGame
 {
+    _nextAsteroidSpawn = 0;
+    
+    for (SKSpriteNode *asteroid in _asteroids) {
+        asteroid.hidden = YES;
+    }
+    
     _ship.hidden = NO;
     //reset ship position for new game
     _ship.position = CGPointMake(self.frame.size.width * 0.1, CGRectGetMidY(self.frame));
+    
+    for (SKSpriteNode *laser in _shipLasers) {
+        laser.hidden = YES;
+    }
     
     //setup to handle accelerometer readings using CoreMotion Framework
     [self startMonitoringAcceleration];
@@ -122,8 +172,42 @@
 {
     CMAccelerometerData* data = _motionManager.accelerometerData;
     if (fabs(data.acceleration.x) > 0.2) {
-        NSLog(@"acceleration value = %f",data.acceleration.x);
+        [_ship.physicsBody applyForce:CGVectorMake(0.0, 40.0 * data.acceleration.x)];
     }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+    //1
+    SKSpriteNode *shipLaser = [_shipLasers objectAtIndex:_nextShipLaser];
+    _nextShipLaser++;
+    if (_nextShipLaser >= _shipLasers.count) {
+        _nextShipLaser = 0;
+    }
+    
+    //2
+    shipLaser.position = CGPointMake(_ship.position.x+shipLaser.size.width/2,_ship.position.y+0);
+    shipLaser.hidden = NO;
+    [shipLaser removeAllActions];
+    
+    //3
+    CGPoint location = CGPointMake(self.frame.size.width, _ship.position.y);
+    SKAction *laserMoveAction = [SKAction moveTo:location duration:0.5];
+    //4
+    SKAction *laserDoneAction = [SKAction runBlock:(dispatch_block_t)^() {
+        //NSLog(@"Animation Completed");
+        shipLaser.hidden = YES;
+    }];
+    
+    //5
+    SKAction *moveLaserActionWithDone = [SKAction sequence:@[laserMoveAction,laserDoneAction]];
+    //6
+    [shipLaser runAction:moveLaserActionWithDone withKey:@"laserFired"];
+    
+}
+
+- (float)randomValueBetween:(float)low andValue:(float)high {
+    return (((float) arc4random() / 0xFFFFFFFFu) * (high - low)) + low;
 }
 
 -(void)update:(NSTimeInterval)currentTime {
@@ -133,6 +217,65 @@
     [_parallaxSpaceDust update:currentTime];
     [_parallaxNodeBackgrounds update:currentTime];
     [self updateShipPositionFromMotionManager];
+    double curTime = CACurrentMediaTime();
+    if (curTime > _nextAsteroidSpawn) {
+        //NSLog(@"spawning new asteroid");
+        float randSecs = [self randomValueBetween:0.20 andValue:1.0];
+        _nextAsteroidSpawn = randSecs + curTime;
+        
+        float randY = [self randomValueBetween:0.0 andValue:self.frame.size.height];
+        float randDuration = [self randomValueBetween:2.0 andValue:10.0];
+        
+        SKSpriteNode *asteroid = [_asteroids objectAtIndex:_nextAsteroid];
+        _nextAsteroid++;
+        
+        if (_nextAsteroid >= _asteroids.count) {
+            _nextAsteroid = 0;
+        }
+        
+        [asteroid removeAllActions];
+        asteroid.position = CGPointMake(self.frame.size.width+asteroid.size.width/2, randY);
+        asteroid.hidden = NO;
+        
+        CGPoint location = CGPointMake(-self.frame.size.width-asteroid.size.width, randY);
+        
+        SKAction *moveAction = [SKAction moveTo:location duration:randDuration];
+        SKAction *doneAction = [SKAction runBlock:(dispatch_block_t)^() {
+            //NSLog(@"Animation Completed");
+            asteroid.hidden = YES;
+        }];
+        
+        SKAction *moveAsteroidActionWithDone = [SKAction sequence:@[moveAction, doneAction ]];
+        [asteroid runAction:moveAsteroidActionWithDone withKey:@"asteroidMoving"];
+    }
+    //check for laser collision with asteroid
+    for (SKSpriteNode *asteroid in _asteroids) {
+        if (asteroid.hidden) {
+            continue;
+        }
+        for (SKSpriteNode *shipLaser in _shipLasers) {
+            if (shipLaser.hidden) {
+                continue;
+            }
+            
+            if ([shipLaser intersectsNode:asteroid]) {
+                shipLaser.hidden = YES;
+                asteroid.hidden = YES;
+                
+                NSLog(@"you just destroyed an asteroid");
+                continue;
+            }
+        }
+        if ([_ship intersectsNode:asteroid]) {
+            asteroid.hidden = YES;
+            SKAction *blink = [SKAction sequence:@[[SKAction fadeOutWithDuration:0.1],
+                                                   [SKAction fadeInWithDuration:0.1]]];
+            SKAction *blinkForTime = [SKAction repeatAction:blink count:4];
+            [_ship runAction:blinkForTime];
+            _lives--;
+            NSLog(@"your ship has been hit!");
+        }
+    }
 }
 
 @end
